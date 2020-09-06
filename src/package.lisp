9a4537cfcf04d669d3e201e2834011104d58e1d7
Aaron France <aaron.l.france@gmail.com>
2020-09-06 17:22:26 +0200

    it puts the library in the page table or it gets the hose again

--- src/package.lisp
+++ src/package.lisp
@@ -2,7 +2,7 @@
   (:use :cl :cffi))
 
 (cffi:define-foreign-library lsquic
-  (:unix (:or "lsquic/liblsquic/liblsquic.so")))
+  (:unix (:or "lsquic/src/liblsquic/liblsquic.so")))
 
 (defun lispify (name flag &optional (package *package*))
   "Borrowed from: https://github.com/mtstickney/cl-libqmlbind/blob/24df1d4248c8962eaadb9dc180e25afa14c7492d/cl-libqmlbind.lisp"
