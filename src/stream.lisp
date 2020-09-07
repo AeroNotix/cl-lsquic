9c62a5aeba2f9f7866e79a7665f82f28a48ed43e
Aaron France <aaron.l.france@gmail.com>
2020-09-07 16:52:32 +0200

    sketch stream API

---
+++ src/stream.lisp
@@ -0,0 +1,9 @@
+(in-package :lsquic)
+
+(defgeneric stream-read (stream buf count)
+  (:documentation "An implementation of a stream of data, writes count bytes to buf"))
+
+(defgeneric stream-remaining (stream)
+  (:documentation "Returns the amount of bytes remaining on a stream"))
+
+(defclass streamer () ())
