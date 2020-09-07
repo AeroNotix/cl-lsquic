1501799e76bab2e40ca22709e647276c074f86ca
Aaron France <aaron.l.france@gmail.com>
2020-09-07 13:30:52 +0200

    remove unused muffs in lispify

--- src/package.lisp
+++ src/package.lisp
@@ -10,15 +10,11 @@
 
 (defun lispify (name flag &optional (package *package*))
   "Borrowed from: https://github.com/mtstickney/cl-libqmlbind/blob/24df1d4248c8962eaadb9dc180e25afa14c7492d/cl-libqmlbind.lisp"
-  (let ((muffs (case flag
-                 ((constant enum) "+")
-                 (variable "*")
-                 (t ""))))
-    (intern (string-upcase (map 'string (lambda (c)
-                                          (case c
-                                            (#\_ #\-)
-                                            (t c)))
-                                name))
-            package)))
+  (intern (string-upcase (map 'string (lambda (c)
+                                        (case c
+                                          (#\_ #\-)
+                                          (t c)))
+                              name))
+          package))
 
 (cffi:use-foreign-library lsquic)
