748cee9ac2f20970c5d233d497025fac62116c61
Aaron France <aaron.l.france@gmail.com>
2020-09-08 02:08:44 +0200

    fix logging

--- src/logging.lisp
+++ src/logging.lisp
@@ -2,11 +2,12 @@
 
 
 (defcallback my-log-buf :int ((ctx :pointer) (buf :pointer) (len :int))
-  ;; (format t "~A~%" (foreign-string-to-lisp buf :count len))
-  )
+  (format t "~A~%" (foreign-string-to-lisp buf :count len))
+  len)
 
 (defparameter logger-if
   (progn
     (let ((logger (foreign-alloc '(:struct lsquic-logger-if))))
       (with-foreign-slots ((log-buf) logger (:struct lsquic-logger-if))
-        (setf log-buf (callback my-log-buf))))))
+        (setf log-buf (callback my-log-buf)))
+      logger)))
