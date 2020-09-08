b49bfff1af2d5f5db19509a5835187041aae2a69
Aaron France <aaron.l.france@gmail.com>
2020-09-09 01:14:59 +0200

    Connect a UDP socket and generate the proper sockaddrs for it

--- lsquic.asd
+++ lsquic.asd
@@ -6,6 +6,7 @@
   :description "FFI Bindings to litespeed QUIC"
   :components ((:file "src/package")
                (:file "src/ffi")
+               (:file "src/ffi-dns")
                (:file "src/tyranny")
                (:file "src/logging")
                (:file "src/lsquic")
--- src/sockets.lisp
+++ src/sockets.lisp
@@ -1,5 +1,9 @@
 (in-package :lsquic)
 
+(defclass socket ()
+  ((socket :initarg :socket :accessor socket)
+   (local-sockaddr :initarg :local-sockaddr :accessor local-sockaddr)
+   (peer-sockaddr :initarg :peer-sockaddr :accessor peer-sockaddr)))
 
 (defun get-peer-address-for-host (host)
   (let ((addrs (multiple-value-list (sb-bsd-sockets:get-host-by-name host))))
@@ -15,8 +19,27 @@
 (defun ip-to-byte-array (ip)
   (parse-ip-from-string ip #\. (byte 8 0)))
 
+(defun ip-vec-to-string (ip)
+  (format nil "~{~D~^.~}" (coerce ip 'list)))
+
+(defun as-sockaddr (socket-family ip-port)
+  (let* ((ip (nth 0 ip-port))
+         (port (nth 1 ip-port))
+         (ip-str (ip-vec-to-string ip)))
+    (with-pointer-to-int (p port)
+      (gen-sockaddr socket-family ip-str p))))
 
 (defun create-udp-socket (host &key (port 443))
-  (let ((socket (make-instance 'sb-bsd-sockets:inet-socket :type :datagram :protocol :udp))
-        (addr (nth 0 (get-peer-address-for-host host))))
-    (sb-bsd-sockets:socket-connect socket (sb-bsd-sockets:host-ent-address addr) port)))
+  (let* ((socket (make-instance 'sb-bsd-sockets:inet-socket :type :datagram :protocol :udp))
+         (addr (nth 0 (get-peer-address-for-host host)))
+         (socket-family (sb-bsd-sockets:socket-family socket)))
+    (sb-bsd-sockets:socket-connect socket (sb-bsd-sockets:host-ent-address addr) port)
+    (let ((local-sockaddr (as-sockaddr socket-family (multiple-value-list (sb-bsd-sockets:socket-peername socket))))
+          (peer-sockaddr (as-sockaddr socket-family (multiple-value-list (sb-bsd-sockets:socket-peername socket)))))
+      (make-instance 'socket :socket socket :local-sockaddr local-sockaddr :peer-sockaddr peer-sockaddr))))
+
+(defmethod close-udp-socket ((socket socket))
+  (with-slots (local-sockaddr peer-sockaddr) socket
+    (foreign-free local-sockaddr)
+    (foreign-free peer-sockaddr)))
+
