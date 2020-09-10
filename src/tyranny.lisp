6486d439999a4fe912266d9534cd556a62209dea
Aaron France <aaron.l.france@gmail.com>
2020-09-10 02:38:01 +0200

    fix: delouse tyranny in macros

--- src/tyranny.lisp
+++ src/tyranny.lisp
@@ -1,22 +1,26 @@
 (in-package :lsquic)
 
-(defun make-pointer-to-int (int-value)
-  (cffi:foreign-alloc :int :initial-element int-value))
-
+(defun make-pointer-to (type val)
+  (cffi:foreign-alloc type :initial-element val))
 
 ;; TODO: Seriously? This is bullshit. Need a better way.
 ;;
 ;; CFFI requires that all arguments to c-funcs are pointers. When a
 ;; c-func takes a simple scalar value you end up with silly things
 ;; like this.
-(defmacro with-pointer-to-int (binding-form &body body)
-  (let ((name (first binding-form))
-        (value (second binding-form)))
-    `(let ((,name (make-pointer-to-int ,value)))
+
+(defmacro with-pointer-to (binding-form &body body)
+  (let ((name-value (first binding-form))
+        (type (second binding-form)))
+    `(let ((,(first name-value) (make-pointer-to ,type ,(second name-value))))
        (unwind-protect
             (progn
               ,@body)
-         (cffi:foreign-free ,name)))))
+         (cffi:foreign-free ,(first name-value))))))
+
+(defmacro with-pointer-to-int (binding-form &body body)
+  `(with-pointer-to (,binding-form :int)
+     ,@body))
 
 (defmacro safe-foreign-alloc (type)
   `(static-vectors:fill-foreign-memory (foreign-alloc ,type) (foreign-type-size ,type) 0))
