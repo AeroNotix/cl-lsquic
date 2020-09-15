6a30a7f203c6a7a0bb31b7e34722d3b0d5a30381
Aaron France <aaron.l.france@gmail.com>
2020-09-15 23:08:42 +0200

    add simple pipe implementation

--- src/lsquic/client.lisp
+++ src/lsquic/client.lisp
@@ -25,6 +25,23 @@
         (foreign-enum-value 'version key)
         (error 'invalid-quic-version :requested str :available version-map))))
 
+(defclass pipe ()
+  ((input :accessor input)
+   (output :accessor output)
+   (ctx :initarg :ctx :accessor ctx)))
+
+(defmethod initialize-instance :after ((pipe pipe) &key)
+  (multiple-value-bind (i o) (sb-posix:pipe)
+    (setf (input pipe) i)
+    (setf (output pipe) o)))
+
+(defmethod close-pipe ((pipe pipe))
+  (sb-posix:close (input pipe))
+  (sb-posix:close (output pipe)))
+
+(defcallback write-pipe :void ((ctx :pointer) (buf :pointer) (len :int) (fin :int))
+  (sb-posix:write (output (weird-pointers:restore ctx)) buf len))
+
 (defclass client (socket)
   ((rq-lock         :initform (bt:make-lock) :accessor rq-lock)
    (request-queue   :initform '() :accessor request-queue)
