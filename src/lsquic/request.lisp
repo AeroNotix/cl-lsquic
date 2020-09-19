d3f660b8ed8f9c0a9a213e6881aa771111277647
Aaron France <aaron.l.france@gmail.com>
2020-09-19 20:46:36 +0200

    tidy request

--- src/lsquic/request.lisp
+++ src/lsquic/request.lisp
@@ -5,35 +5,6 @@
   ((name :initarg :name :accessor name)
    (value :initarg :value :accessor value)))
 
-(defmethod make-lsxpack-header ((header header))
-  (format t "name len ~D value len ~D~%" (length (name header)) (length (value header)))
-  (let ((lheader (with-initialize-foreign-struct lsquic:lsxpack-header
-                   (setf lsquic:buf (foreign-string-alloc (format nil "~A~A" (name header) (value header)) :null-terminated-p nil))
-                   (setf lsquic:name-len (length (name header)))
-                   (setf lsquic:name-offset 0)
-                   (setf lsquic:val-len (length (value header)))
-                   (setf lsquic:val-offset (length (name header)))
-                   )))
-
-    (with-foreign-slots ((name-len name-offset val-len val-offset) lheader (:struct lsquic:lsxpack-header))
-        (format t "name len ~D name offset ~D value len ~D value offset ~D~%" name-len name-offset val-len val-offset))
-
-    ;; (let ((buf (foreign-string-alloc (format nil "~A~A" (name header) (value header)))))
-
-    ;;   (lsxpack-header-set-offset2
-    ;;    lheader
-    ;;    buf
-    ;;    0
-    ;;    (length (name header))
-    ;;    (length (name header))
-    ;;    (length (value header))))
-
-    ;; (with-foreign-slots ((name-len name-offset val-len val-offset) lheader (:struct lsquic:lsxpack-header))
-    ;;   (format t "name len ~D name offset ~D value len ~D value offset ~D~%" name-len name-offset val-len val-offset))
-    (format t "~A~%" (lsxpack-header-get-name lheader))
-    (format t "~A~%" (lsxpack-header-get-value lheader))
-    lheader))
-
 (defclass request ()
   ((path :initarg :path :accessor path)
    (scheme :initarg :scheme :accessor scheme)
@@ -41,6 +12,16 @@
    (authority :initarg :authority :accessor authority)
    (verb :initarg :verb :accessor verb)
    (body :initarg :body :accessor body)))
+
+(defmethod make-lsxpack-header ((header header))
+  (with-initialize-foreign-struct lsquic:lsxpack-header
+    (setf lsquic:buf
+          (foreign-string-alloc
+           (format nil "~A~A" (name header) (value header))))
+    (setf lsquic:name-len (length (name header)))
+    (setf lsquic:name-offset 0)
+    (setf lsquic:val-len (length (value header)))
+    (setf lsquic:val-offset (length (name header)))))
 
 (defmethod required-headers ((request request))
   (list
@@ -56,7 +37,6 @@
                            :pointer
                            :count header-count
                            :initial-contents (mapcar #'make-lsxpack-header all-headers))))
-
     (with-initialize-foreign-struct lsquic::http-headers
       (setf lsquic:count header-count)
       (setf lsquic:headers lsxpack-headers) lsxpack-headers)))
