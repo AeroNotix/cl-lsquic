4a696e175a4bc4d9b06b4776bb243c00266a5993
Aaron France <aaron.l.france@gmail.com>
2020-09-20 13:54:38 +0200

    add simple test

--- src/http3/test/tests.lisp
+++ src/http3/test/tests.lisp
@@ -8,3 +8,10 @@
 (test can-create-request
   (let ((request (make-instance 'lsquic:request :authority "google.com")))
     (is (lsquic:lsxpack-headers request))))
+
+(test can-make-network-request
+  (let ((request (make-instance 'lsquic:request :authority "google.com"))
+        (client (make-instance 'lsquic:client
+                               :quic-version "Q050"
+                               :host "google.com")))
+    (http3:connect client)))
