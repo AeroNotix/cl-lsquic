15aa25edd2daccaff3d4b370e0e441b897b5d19f
Aaron France <aaron.l.france@gmail.com>
2020-09-27 23:52:32 +0200

    improve debug helper

--- c-src/lsquic-helpers.c
+++ c-src/lsquic-helpers.c
@@ -5,35 +5,20 @@
 #include "../lsquic/include/lsquic.h"
 #include <stdlib.h>
 
-void view_header(struct lsquic_http_headers *hdr) {
-    unsigned char *p = (unsigned char *)hdr;
-    printf("Header count: %d\n", hdr->count);
-    for (int x = 0; x < hdr->count; x++) {
-        printf("BufP: %p\n", hdr->headers[x].buf);
-        printf("BufS: %s\n", hdr->headers[x].buf);
-        printf("NameLen: %d\n", hdr->headers[x].name_len);
-        printf("NameOff: %d\n", hdr->headers[x].name_offset);
-        printf("ValLen: %d\n", hdr->headers[x].val_len);
-        printf("ValOff: %d\n", hdr->headers[x].val_offset);
-        for (int i = 0; i < sizeof(struct lsxpack_header); i++) {
-            printf("0x%02x ", p[i]);
-        }
-        printf("\n");
-    }
-}
 
 void view_lsxpack_header(struct lsxpack_header *hdr) {
-    unsigned char *p = (unsigned char*) hdr;
-    printf("New lsxpack header:\n");
-    printf("Buf: %p\n", hdr->buf);
-    printf("Buf: %s\n", hdr->buf);
-    printf("Buf: %d\n", hdr->name_len);
-    printf("Buf: %d\n", hdr->name_offset);
-    printf("Buf: %d\n", hdr->val_len);
-    printf("Buf: %d\n", hdr->val_offset);
-    printf("Buf: %d\n", hdr->buf == hdr);
-    for (int i = 0; i < sizeof(struct lsxpack_header); i++)
-        printf("0x%02x ", p[i]);
-    printf("\n");
+    printf("hdr: %p\n", hdr);
+    printf("BufP: %p\n", hdr->buf);
+    printf("BufS: %s\n", hdr->buf);
+    printf("NameLen: %d\n", hdr->name_len);
+    printf("NameOff: %d\n", hdr->name_offset);
+    printf("ValLen: %d\n", hdr->val_len);
+    printf("ValOff: %d\n", hdr->val_offset);
+}
 
+void view_header(struct lsquic_http_headers *hdr) {
+    printf("Header count: %d | %ld\n", hdr->count, sizeof(struct lsxpack_header));
+    for (int x = 0; x < hdr->count; x++) {
+        view_lsxpack_header(&(hdr->headers[x]));
+    }
 }
