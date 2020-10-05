65cf4a3c4cb29839bef34653e76011ed6c18a667
Aaron France <aaron.l.france@gmail.com>
2020-10-05 02:20:41 +0200

    switch to libev fully

--- src/lsquic/ev.lisp
+++ src/lsquic/ev.lisp
@@ -1,7 +1,16 @@
 (in-package :lsquic)
 
+(defun calculate-next-tick (diff)
+  (coerce (if (>= diff DF-CLOCK-GRANULARITY)
+              (/ diff 1000000)
+              (if (<= diff 0)
+                  5d0
+                  (/ DF-CLOCK-GRANULARITY 1000000)))
+          'double-float))
+
 (defcallback process-conns-cb :void ((evloop :pointer) (timer :pointer) (revents :int))
   (declare (ignore revents))
+  (format t "process-conns-cb~%")
   (with-foreign-slots ((lev::data) timer (:struct lev:ev-timer))
     (let ((client (weird-pointers:restore lev::data)))
       (with-slots (engine) client
@@ -9,11 +18,12 @@
         (lsquic:engine-process-conns engine)
         (with-pointer-to-int (diff 0)
           (when (> (lsquic:engine-earliest-adv-tick engine diff) 0)
-            (lev:ev-timer-init evloop (callback process-conns-cb) diff 0)))))))
+            (format t "diff ~D~%" (calculate-next-tick (cffi:mem-ref diff :int)))
+            (lev:ev-timer-init timer 'process-conns-cb (calculate-next-tick (cffi:mem-ref diff :int)) 0d0)
+            (lev:ev-timer-start evloop timer)))))))
 
 (defcallback packets-in-cb :void ((evloop :pointer) (io :pointer) (revents :int))
   (declare (ignore revents evloop))
-  ;; TODO: Switch to libev
   (with-foreign-slots ((lev::data) io (:struct lev:ev-io))
     (let ((client (weird-pointers:restore lev::data)))
       (with-slots (socket engine peer-ctx) client
